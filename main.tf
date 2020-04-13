4265a6012d339887efc0c0ec58c562167a064923
pgfisico <pgfisico.dev@gmail.com>
2020-04-12 22:25:02 -0600

    Add initial main.tf (#5)
    Closes #3

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-west-2"
+}
+
+data "terraform_remote_state" "dns" {
+  backend = "remote"
+  config = {
+    hostname = "app.terraform.io"
+    organization = "intimitrons"
+    workspaces = {
+      name = "dns"
+    }
+  }
+}
