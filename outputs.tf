4ff33ca8038e4b7056a5463889e2bc57825da972
pgfisico <pgfisico.dev@gmail.com>
2020-04-25 17:31:48 -0600

    Create CloudFront distribution (#13)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "web_bucket_website_endpoint" {
   value       = aws_s3_bucket.web_bucket.website_endpoint
   description = "The S3 website endpoint for the web bucket"
 }
+
+output "web_distribution_domain_name" {
+  value       = aws_cloudfront_distribution.web_distribution.domain_name
+  description = "The domain name of the CloudFront distribution"
+}
